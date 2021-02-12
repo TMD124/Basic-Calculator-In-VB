367344cb636122455da545bab085cf8ffe82e44c
THEMLGDOCTOR12 <75111234+THEMLGDOCTOR12@users.noreply.github.com>
2021-02-12 11:08:55 +1100

    Add files via upload

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.cmbOperation = New System.Windows.Forms.ComboBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(12, 12)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(83, 23)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(190, 12)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(83, 23)
+        Me.txtNum2.TabIndex = 1
+        '
+        'cmbOperation
+        '
+        Me.cmbOperation.FormattingEnabled = True
+        Me.cmbOperation.Items.AddRange(New Object() {"Plus", "Minus", "Multiply", "Divide"})
+        Me.cmbOperation.Location = New System.Drawing.Point(101, 12)
+        Me.cmbOperation.Name = "cmbOperation"
+        Me.cmbOperation.Size = New System.Drawing.Size(83, 23)
+        Me.cmbOperation.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Location = New System.Drawing.Point(287, 16)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(16, 16)
+        Me.TextBox3.TabIndex = 3
+        Me.TextBox3.Text = "="
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(309, 54)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(83, 23)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.BackColor = System.Drawing.SystemColors.Window
+        Me.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAnswer.Location = New System.Drawing.Point(309, 14)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(83, 23)
+        Me.lblAnswer.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 89)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.cmbOperation)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.Text = "Do Calculations"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents cmbOperation As ComboBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lbl As Label
+    Friend WithEvents lblAnswer As Label
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dbl1stNum As Double
+        Dim dbl2stNum As Double
+        Dim dblSumNum As Double
+        Dim strCombo As String
+
+        dbl1stNum = Val(txtNum1.Text)
+        dbl2stNum = Val(txtNum2.Text)
+        strCombo = cmbOperation.Text
+
+        If strCombo = "Multiply" Then
+            dblSumNum = dbl1stNum * dbl2stNum
+        ElseIf strCombo = "Divide" Then
+            dblSumNum = dbl1stNum / dbl2stNum
+        ElseIf strCombo = "Plus" Then
+            dblSumNum = dbl1stNum + dbl2stNum
+        ElseIf strCombo = "Minus" Then
+            dblSumNum = dbl1stNum - dbl2stNum
+        End If
+        lblAnswer.Text = dblSumNum
+    End Sub
+End Class
